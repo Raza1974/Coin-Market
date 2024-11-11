6c72ae2f6dc71f15070ac4d06d408fc81bcdb727
Asim74 <asimr6573@gmail.com>
2024-11-11 07:57:24 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 "use client";
+import CoinMarket from "./coin-market/page";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-between items-center">
-      Check Coin-Market Page
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+      <h1 className="text-4xl font-bold mb-8 text-center">Welcome to the Coin Market</h1>
+      <p className="mb-5 text-lg text-gray-300">Check the latest coin market prices</p>
+      <div className="w-full max-w-md p-5 bg-gray-900 rounded-lg shadow-lg">
+        <CoinMarket />
+      </div>
     </div>
   );
 }
